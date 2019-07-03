1f1478ae951b3d401c88e8a969eade5369985695
RadekKosiada <kosiadar@gmail.com>
2019-07-04 00:30:29 +0200

    Change App.js to state component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.css';
-import Results from './components/results';
+import React, { Component } from "react";
+import "./App.css";
+import Results from "./components/results";
+import Map from "./components/map";
 
-function App() {
-  return (
-    <div className="App">      
-      <Results />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: "",
+      longitude: ""
+    };
+  }
+  
+  render() {
+    console.log(latitude, longitude)
+    return (
+      <div className="App">
+        <Results />
+        <Map />
+      </div>
+    );
+  }
 }
 
 export default App;
