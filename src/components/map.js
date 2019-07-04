b4af8fe7dc5375dc0e117a2b96561b972ddbad5f
RadekKosiada <kosiadar@gmail.com>
2019-07-04 15:34:27 +0200

    Delete external function & render map AND marker in componentDidMount()

--- src/components/map.js
+++ src/components/map.js
@@ -23,13 +23,24 @@ class Map extends Component {
     // console.log(this.state.lat, this.state.lng);
     const lat = this.state.lat;
     const lng = this.state.lng;
+    L.marker([51.0834196, 10.4234469]).addTo(map);
     // drawMap(49.8419, 24.0315)
    
   }
 
   componentDidMount() {
     //drawMap with lat & lng of Germany
-    drawMap(51.0834196, 10.4234469)
+    this.map = L.map('map', {
+      center: [51.0834196, 10.4234469],
+      zoom: 6,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });
+    //adding marker in the middle of DE
+    this.marker = L.marker([51.0834196, 10.4234469]).addTo(this.map);
   }
 
   render() {
@@ -40,19 +51,19 @@ class Map extends Component {
 
 export default Map;
 
-function drawMap(lat, lng) {
-  // console.log(document.getElementById('map'))
-  const map = L.map('map', {
-    center: [lat, lng],
-    zoom: 6,
-    layers: [
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }),
-    ]
-  });
+// function drawMap(lat, lng) {
+//   // console.log(document.getElementById('map'))
+//   const map = L.map('map', {
+//     center: [lat, lng],
+//     zoom: 6,
+//     layers: [
+//       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//       }),
+//     ]
+//   });
   
-  return map;
-}
+//   return map;
+// }
 
 
