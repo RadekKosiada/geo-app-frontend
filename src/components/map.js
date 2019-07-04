511cac0be7a9815229d2a0dd096ad19aa0adab40
RadekKosiada <kosiadar@gmail.com>
2019-07-04 15:24:45 +0200

    Render map of DE when page loads

--- src/components/map.js
+++ src/components/map.js
@@ -13,30 +13,23 @@ class Map extends Component {
   
   componentDidUpdate({geoObject, map}) {
     if (this.props.geoObject !== geoObject) {
-     console.log(this.props.geoObject)
+    //  console.log(this.props.geoObject)
      this.setState({
       // lat: this.props.geoObject.lat,
       // lng: this.props.geoObject.lng,
-      map: drawMap(this.props.geoObject.lat, this.props.geoObject.lng)
     });
     
     }
-    console.log(this.state.lat, this.state.lng);
+    // console.log(this.state.lat, this.state.lng);
     const lat = this.state.lat;
     const lng = this.state.lng;
     // drawMap(49.8419, 24.0315)
+   
   }
 
   componentDidMount() {
-    console.log(this.state.lat, this.state.lng, this.props);
-
-    // create map
-   
-    this.setState({
-      // map: drawMap(49.8419, 24.0315)
-    }) 
-    console.log(this.state.map);
-    console.log((document.getElementById('map')==true))
+    //drawMap with lat & lng of Germany
+    drawMap(51.0834196, 10.4234469)
   }
 
   render() {
@@ -48,16 +41,17 @@ class Map extends Component {
 export default Map;
 
 function drawMap(lat, lng) {
-  console.log(document.getElementById('map'))
+  // console.log(document.getElementById('map'))
   const map = L.map('map', {
     center: [lat, lng],
-    zoom: 16,
+    zoom: 6,
     layers: [
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }),
     ]
   });
+  
   return map;
 }
 
