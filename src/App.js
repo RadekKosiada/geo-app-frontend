5eeb129010167480aa58ca03d17f316295e0940c
RadekKosiada <kosiadar@gmail.com>
2019-07-04 20:21:50 +0200

    Render Map with react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import Results from "./components/results";
-import Map from "./components/map";
-import axios from "axios"; 
+import MapComponent from "./components/map";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
@@ -10,18 +10,27 @@ class App extends Component {
     this.state = {
       geoObject: {},
       value: "",
-      showInput : true
+      showInput: true,
+      markerPosition: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.submitOnEnter = this.submitOnEnter.bind(this);
-    this.activateEditMode = this.activateEditMode.bind(this);
+    this.editSearchQuery = this.editSearchQuery.bind(this);
     this.deleteSearchQuery = this.deleteSearchQuery.bind(this);
+    this.testData = this.testData.bind(this);
   }
-  componentDidMount() {
+
+
+  renderData() {
     fetch("/api")
       .then(res => res.json())
-      .then(geolocation => this.setState({ geoObject: geolocation }))
+      .then(geolocation =>
+        this.setState({
+          geoObject: geolocation,
+          markerPosition: [geolocation.lat, geolocation.lng]
+        })
+      )
       .catch(err => console.log);
   }
 
@@ -30,17 +39,21 @@ class App extends Component {
     console.log("Submit fired!");
     this.setState({
       showInput: false
-    })
+    });
     //submitting searchQuery to the server
-    axios.post('/submitQuery', {
-      searchQuery: this.state.value
-  })
-  .then(response => console.log(response))
-  .catch(err => console.log("Error in submitting query to backend: ", err.message))
+    axios
+      .post("/submitQuery", {
+        searchQuery: this.state.value
+      })
+      .then(response => console.log(response))
+      .catch(err =>
+        console.log("Error in submitting query to backend: ", err.message)
+      );
+    this.renderData();
   }
 
   submitOnEnter(e) {
-    if(e.key === 'Enter') {
+    if (e.key === "Enter") {
       this.handleSubmit(e);
     } else {
       return;
@@ -51,33 +64,46 @@ class App extends Component {
       value: e.target.value
     });
   }
-  activateEditMode() {
+  editSearchQuery() {
     this.setState({
-      showInput: true,
-    })
+      showInput: true
+    });
   }
   deleteSearchQuery() {
     this.setState({
       showInput: true,
-      value: ''
-    })
+      value: "",
+      markerPosition: "",
+      geoObject: ""
+    });
+  }
+  testData() {
+    console.log(this.state.markerPosition)
   }
   render() {
     return (
       <div className="App">
-         {this.state.showInput && (<form className="" onSubmit={this.handleSubmit}>
-         <input type="text" autoComplete="off" placeholder="your desired location" value={this.state.value} name="answer" onKeyPress={this.submitOnEnter} onChange={this.handleChange} />
-          <input className="button" type="button" value="Submit" />
-          </form>)}
-
-        {!this.state.showInput && (<h3>{this.state.value}</h3>)}
-
-          
-        
-        <Results geoObject={this.state.geoObject} value={this.state.value} />
-        <button onClick={this.activateEditMode}>Edit</button>or
-        <button onClick={this.deleteSearchQuery}>Delete</button><br />
-        <Map geoObject={this.state.geoObject} />
+        {this.state.showInput && (
+          <form className="" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              autoComplete="off"
+              placeholder="your desired location"
+              value={this.state.value}
+              name="answer"
+              onKeyPress={this.submitOnEnter}
+              onChange={this.handleChange}
+            />
+            <input className="button" type="button" value="Submit" />
+          </form>
+        )}
+        {!this.state.showInput && <h3>{this.state.value}</h3>}
+        <Results geoObject={this.state.geoObject} markerPosition={this.state.markerPosition} value={this.state.value} />
+        <button onClick={this.editSearchQuery}>Edit</button>or
+        <button onClick={this.deleteSearchQuery}>Delete</button>
+        <button onClick={this.testData}>TEST</button>
+        <br />
+        <MapComponent markerPosition={this.state.markerPosition} />
       </div>
     );
   }
--- src/components/map.js
+++ src/components/map.js
@@ -1,43 +1,59 @@
 import React, { Component } from "react";
 import L from "leaflet";
+import { Map, CircleMarker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const germanLat = 51.0834196;
 const germanLng = 10.4234469;
-class Map extends Component {
+
+class MapComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {  
-    
-
-    };
+    // this.state = {
+    //   markerPosition: ''
+    // };
   }
-  
   componentDidUpdate() {
-    
+    console.log(this.props.markerPosition);
+    // this.setState({
+    //   markerPosition: this.props.markerPosition
+    // })
   }
-
   componentDidMount() {
-    //drawMap with lat & lng of Germany
-    this.map = L.map('map', {
-      center: [germanLat, germanLng],
-      zoom: 6,
-      layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
-      ]
-    });
-    //adding marker in the middle of DE
-    this.marker = L.marker(this.props.markerPosition).addTo(this.map);
+    console.log(this.props.markerPosition);
   }
-
   render() {
-
-    return <div id="map" />;
+    return (
+      <Map
+        style={{ height: "380px", width: "80%", display: "inline-block" }}
+        zoom={5}
+        center={[germanLat, germanLng]}
+      >
+        <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* {this.props.markerPosition && (<CircleMarker center={this.props.markerPosition} />)} */}
+        {/* {this.props.markerPosition &&(<Marker markerPosition={this.props.markerPosition} />)} */}
+        <Marker markerPosition={this.props.markerPosition} />
+      </Map>
+    );
   }
 }
 
-export default Map;
+export default MapComponent;
 
+class Marker extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentWillUpdate() {
 
-
+  }
+  render() {
+    if(this.props.markerPosition) {
+      return (
+        <CircleMarker center={this.props.markerPosition} />
+      )
+    } else return (
+      <div></div>
+    )
+    }
+}
