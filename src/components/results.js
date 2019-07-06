45518bc28e6e621bd6691fd954ee197d568b61be
RadekKosiada <kosiadar@gmail.com>
2019-07-06 16:06:50 +0200

    Adjust displaying coordinates

--- src/components/results.js
+++ src/components/results.js
@@ -2,15 +2,27 @@ import React from "react";
 
 export default function Results(props) {
   const coordinates = props.markerPosition;
+  const noData = 'waiting for the data'
 
-  return (    
+  return (
     <div className="results">
       <p />
       {/* {props.value && (<p>{props.value}</p>)}
       {!props.value && (<p>your location</p>)} */}
       <h3>{props.geoObject.type}</h3>
-      <p>Latitude: {coordinates[0]}</p>
-      <p>Longitude: {coordinates[1]}</p>
+      {!coordinates && (
+        <div>
+          <p>Latitude: <span className="no-data">{noData}</span></p>
+          <p>Longitude: <span className="no-data">{noData}</span></p>
+        </div>
+      )}
+
+      {coordinates && (
+        <div>
+          <p>Latitude: {coordinates[0]}</p>
+          <p>Longitude: {coordinates[1]}</p>
+        </div>
+      )}
     </div>
   );
 }
