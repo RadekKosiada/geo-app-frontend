ef1d1246d55a54c9b23e154c44683adcce0ea44a
RadekKosiada <kosiadar@gmail.com>
2019-07-07 19:49:33 +0200

    Clean results.js

--- src/components/results.js
+++ src/components/results.js
@@ -2,33 +2,32 @@ import React from "react";
 
 export default function Results(props) {
   const coordinates = props.markerPosition;
-  const noData = 'waiting for the data';
 
   return (
     <div className="">
       <h1 className={`destination ${props.showInput ? 'invisible' : null}`}>{props.value}</h1>
 
       {coordinates && (
-        <div id="coord1">
+        <div>
           <h4>{props.geoObject.type}</h4>
           <p>Latitude: {coordinates[0]}</p>
           <p>Longitude: {coordinates[1]}</p>
         </div>
       )} 
 
-      {!coordinates && (
-        <EmptyResults string={noData} />
-      )}
+      {!coordinates && ( 
+        <div>
+          <h4>type: <EmptyResults /></h4> 
+          <p>Latitude: <EmptyResults /></p>
+          <p>Longitude: <EmptyResults /></p>
+        </div>)}
+
     </div>
   );
 }
 
-function EmptyResults(props) {
-  return (
-    <div>
-      <h4>type: <span className="no-data">{props.string}</span></h4>
-      <p>Latitude: <span className="no-data">{props.string}</span></p>
-      <p>Longitude: <span className="no-data">{props.string}</span></p>
-    </div>
+function EmptyResults() {
+  return (   
+      <span className="no-data">waiting for the data</span>
   )
 }
