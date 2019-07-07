eb813a5064ecc5459d66d983473b1c9f398926f8
RadekKosiada <kosiadar@gmail.com>
2019-07-07 19:39:46 +0200

    Change map.js to stateless component

--- src/components/map.js
+++ src/components/map.js
@@ -1,52 +1,36 @@
-import React, { Component } from "react";
-import L from "leaflet";
+import React from "react";
 import { Map, CircleMarker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+//define coordinates so the map opens on Germany by default
 const germanLat = 51.0834196;
 const germanLng = 10.4234469;
 
-class MapComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidUpdate() {
-  
-  }
-  render() {
-    return (
-      <Map
-        className="map"
-        style={{ height: "400px", width: "80%", display: "inline-block" }}
-        zoom={5}
-        center={[germanLat, germanLng]}
-      >
-        <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+function MapComponent(props) {
+  return (
+    <Map
+      className="map"
+      style={{ height: "400px", width: "80%", display: "inline-block" }}
+      zoom={5}
+      center={[germanLat, germanLng]}
+    >
+      <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        {this.props.markerPosition1[0] && ( <Marker1 markerPosition1={this.props.markerPosition1} />  )}
-        {this.props.markerPosition2[0] && ( <Marker2 markerPosition2={this.props.markerPosition2} />  )}
+      {props.markerPosition1[0] && (<Marker1 markerPosition1={props.markerPosition1} />)}
+      {props.markerPosition2[0] && (<Marker2 markerPosition2={props.markerPosition2} />)}
 
-      </Map>
-    );
-  }
+    </Map>
+  );
 }
-
-// const customMarker = L.icon({ 
-//   iconUrl: require('medwing.png'),
-//   iconSize: [38, 95], 
-//   opacity: 0.5,
-//   color: 'red'
-//  })
 
 export default MapComponent;
 
-//As coordinates for both Markers will be triggered dynamically I decided to create two seperate components for each of them
 function Marker1(props) {
-    return (
-      <div>
-        <CircleMarker center={props.markerPosition1} />
-      </div>
-    )
+  return (
+    <div>
+      <CircleMarker center={props.markerPosition1} />
+    </div>
+  )
 }
 
 function Marker2(props) {
